14cc0f0d27e52c3c7792661c84e29eb58af9bbba
Edswel <franckini@yahoo.com>
2024-03-12 14:03:57 +0100

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import HomePage from './pages/HomePage';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='product' element={<Product />} />
+          <Route path='pricing' element={<Pricing />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
       <h1>World View</h1>
+      <p>The world through my lens</p>
     </>
   )
 }
